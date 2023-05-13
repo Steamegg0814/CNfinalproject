ca76bd94d4a59d8a2a72663821d9e6d4c045aa93
Oscar Yao <127667560+o78908@users.noreply.github.com>
2023-05-13 12:23:39 +0800

    Add files via upload
    1.更改了app.route('statics') //18行
    2.更改了return render_template('statics.html', labels=top_publishers, values=publisher_counts,dateLabels=top_publishdates, dateValues=publishdate_counts)//59行
    3.加入 if條件排除None

--- page.py
+++ page.py
@@ -10,14 +10,14 @@ import json
 app = Flask(__name__)
 @app.route('/')
 def index():
-    with open('steam.json', newline='') as f:
+    with open('steam.json', newline='',encoding="utf-8") as f:
         data = json.load(f)
     return render_template('page.html',data = data)
 
 
 @app.route('/statics')
 def stats():
-    with open('steam.json', newline='') as f:
+    with open('steam.json', newline='',encoding="utf-8") as f:
         data = json.load(f)
 
     # 計算每個開發商出現次數
@@ -29,16 +29,34 @@ def stats():
         else:
             publishers[publisher] = 1
 
+    publishdates = {}
+    for i in data:
+        publishdate = i['publishDate']
+        if publishdate in publishdates:
+            publishdates[publishdate] += 1
+        else:
+            publishdates[publishdate] = 1
+
     # 排序開發商出現次數
     sorted_publisher = sorted(publishers.items(), key=lambda x: x[1], reverse=True)
 
     # 取出前10名開發商
-    top_publishers = [publisher[0] for publisher in sorted_publisher[:10]]
+    top_publishers = [publisher[0] for publisher in sorted_publisher[:10]if publisher[0] != "None"]
 
     # 計算前10名開發商的出現次數
-    publisher_counts = [publisher[1] for publisher in sorted_publisher[:10]]
+    publisher_counts = [publisher[1] for publisher in sorted_publisher[:10]if publisher[0] != "None" ]
 
-    return render_template('stats.html', labels=top_publishers, values=publisher_counts)
+#############################################################
+    # 排序開發日期出現次數
+    sorted_publishdate = sorted(publishdates.items(), key=lambda x: x[1], reverse=True)
+
+    # 取出前10名開發日期
+    top_publishdates = [publishdate[0] for publishdate in sorted_publishdate[:10]if publishdate[0] != "None"]
+
+    # 計算前10名開發日期的出現次數
+    publishdate_counts = [publishdate[1] for publishdate in sorted_publishdate[:10]if publishdate[0] != "None"]
+#############################################################
+    return render_template('statics.html', labels=top_publishers, values=publisher_counts,dateLabels=top_publishdates, dateValues=publishdate_counts)
 
 
 
